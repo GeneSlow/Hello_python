ab6f9ac862f186d957a2322f1c3cec805979a5f5
GeneSlow <85553491+GeneSlow@users.noreply.github.com>
2021-07-13 10:46:57 +0800

    Add files via upload

---
+++ 3、文字游戏.py
@@ -0,0 +1,9 @@
+print("woaiyu hdh  =======")
+temp=input("不妨猜一下我现在心里想的啥数字")
+guess=int(temp)
+if guess==8:
+    print("how do you know it")
+    print("no way")
+else:
+    print("haha,the num I think is 8")
+print("game over")
---
+++ 名人名言.py
@@ -0,0 +1,9 @@
+name="albbert einstein"
+print(name +'once said,"A person who never made a mistake never tried anything new.')
+print(name+" "+'once said,"A person who never made a mistake never tried anything new."')
+ 
+print(name.title()+" "+'once said,"A person who never made a mistake never tried anything new."')
+print(name.title()+" "+'once said,\n\t"A person who never made a mistake never tried anything new."')
+
+print(name.title()+"\n\t "+'once said,\n\t"A person who never made a mistake never tried anything new."')
+print(name.title()+"\n\t"+'once said,\n\t"A person who never made a mistake never tried anything new."')
