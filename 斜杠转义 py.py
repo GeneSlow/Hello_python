9e0a4beecdfa11caa6ffe355c58439daf0369f1c
GeneSlow <85553491+GeneSlow@users.noreply.github.com>
2021-07-13 10:42:45 +0800

    Update 斜杠转义 py.py

--- 斜杠转义 py.py
+++ 斜杠转义 py.py
@@ -1,31 +1,10 @@
 Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> print("小甲鱼“*5）
-      
-SyntaxError: EOL while scanning string literal
 >>> print('xuaojieyu '*3)
 xuaojieyu xuaojieyu xuaojieyu 
 >>> print(18393+723923)
 742316
->>> print('37287""i9erm
-      
-SyntaxError: EOL while scanning string literal
->>> print('37287""8920)
-      
-SyntaxError: EOL while scanning string literal
 >>> print('37287""8920')
 37287""8920
->>> print("jfeurn
-      
-SyntaxError: EOL while scanning string literal
->>> print("12313"\"\"
-      
-SyntaxError: unexpected character after line continuation character
->>> print("12313"\"\")
-SyntaxError: unexpected character after line continuation character
->>> print("12313\"\")
-      
-SyntaxError: EOL while scanning string literal
 >>> print("12313\"\"")
 12313""
->>> 
